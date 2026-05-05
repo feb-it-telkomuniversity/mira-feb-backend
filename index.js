2e916749ad452e4689c7c71220ef7788d2f341bd
ahmadsidikrofi <rofidragon71@gmail.com>
2026-05-05 12:19:03 +0700

    fix: cors and delete etag

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const allowedOrigins = [
 ]
 
 const app = express()
-app.disable('etag')
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin) return callback(null, true)
@@ -20,10 +19,12 @@ app.use(cors({
         if (allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
-            callback(new Error('Not allowed by CORS'))
+            callback(new Error(`Origin ${origin} not allowed by CORS`))
         }
     },
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
 }))
 
 app.use(express.json())
